5960b75fabc06b1ebafbeb84c92e2f06f2edf56e
y0geshpatil <yogeshpatil8433@gmail.com.com>
2021-01-06 11:00:54 +0530

    Testing commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var CircularJSON = require('circular-json');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send(CircularJSON.stringify(req));
+});
+
+module.exports = router;
